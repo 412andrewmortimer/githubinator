c1c03f700711ad5c429f9352aa9e96e57c133845
Andrew Mortimer <andrcmdr@protonmail.com>
2019-06-04 14:16:57 -0400

    Fix App Prefix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'dg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Doofenshmirtz Githubinator';
-}
+export class AppComponent {}
