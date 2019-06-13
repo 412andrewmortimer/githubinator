53b816ff651f5ff3a1713aa8390d1435f4c17ef3
Andrew Mortimer <andrcmdr@protonmail.com>
2019-06-13 10:47:00 -0400

    Adds a few more attributes to display with Repos

--- src/app/components/github-repo-index/github-repo-index.component.ts
+++ src/app/components/github-repo-index/github-repo-index.component.ts
@@ -4,9 +4,19 @@ import { Repository } from 'src/app/models';
 @Component({
   selector: 'dg-github-repo-index',
   template: `
-    <div *ngFor="let repo of repos">
-      <mat-card>{{ repo.full_name }}</mat-card>
-    </div>
+    <mat-card *ngFor="let repo of repos">
+      <mat-card-title>{{ repo.name }}</mat-card-title>
+      <mat-card-content>
+        <dl>
+          <dt class="mat-small">Full Name</dt>
+          <dd class="mat-body-2">{{ repo.full_name }}</dd>
+          <dt class="mat-small">Stars</dt>
+          <dd class="mat-body-2">{{ repo.stargazers_count }}</dd>
+          <dt class="mat-small">Last Updated</dt>
+          <dd class="mat-body-2">({{ repo.updated_at | date: 'short' }})</dd>
+        </dl>
+      </mat-card-content>
+    </mat-card>
   `
 })
 export class GithubRepoIndexComponent {
--- src/app/models/repository.model.ts
+++ src/app/models/repository.model.ts
@@ -1,3 +1,9 @@
 export interface Repository {
+  description: string;
+  forks: number;
   full_name: string;
+  name: string;
+  stargazers_count: number;
+  topics: Array<string>;
+  updated_at: string;
 }
